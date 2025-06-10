fd32095852e312580389028555ff1e72055ea52d
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-06-10 12:55:19 -0500

    Update Clase.js

--- backend/models/Clase.js
+++ backend/models/Clase.js
@@ -1,26 +1,21 @@
 const mongoose = require("mongoose");
 
-const claseSchema = new mongoose.Schema(
-  {
-    nombreClase: { type: String, required: true },
-    dias: [
-      {
-        dia: { type: String, required: true },
-        horarioInicio: { type: String, required: true },
-        horarioFin: { type: String, required: true },
-      },
-    ],
-    capacidadMaxima: { type: Number, required: true, default: 10 },
-    entrenador: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Entrenador",
-      required: true,
-    },
-    estado: { type: String, enum: ["activa", "inactiva"], default: "activa" },
-  },
-  {
-    timestamps: true,
-  }
-);
+const claseSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  horario: { type: String, required: true },
+  estado: { type: String, default: "disponible" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+claseSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+claseSchema.pre("findOneAndUpdate", function (next) {
+  this.set({ updatedAt: Date.now() });
+  next();
+});
 
 module.exports = mongoose.model("Clase", claseSchema);
