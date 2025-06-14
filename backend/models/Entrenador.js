bce1905c330244daa87bd7f382a0def9f3e1cfa9
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-06-14 04:31:16 -0500

    Update Entrenador.js

--- backend/models/Entrenador.js
+++ backend/models/Entrenador.js
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
 
+const diaSchema = new mongoose.Schema({
+  dia: String,
+  horarioInicio: String,
+  horarioFin: String,
+});
+
+const claseSchema = new mongoose.Schema({
+  nombreClase: String,
+  capacidadMaxima: { type: Number, default: 10 },
+  dias: [diaSchema],
+});
+
 const entrenadorSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   apellido: { type: String, required: true },
   correo: { type: String, required: true, unique: true },
+  telefono: String,
   especialidad: { type: String, required: true },
+  clases: [claseSchema],
+  creadoPor: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
