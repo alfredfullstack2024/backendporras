11dd2b8ea010276412fa32b67102ac2bc7c27ca3
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-06-24 11:17:24 -0500

    Update Clase.js

--- backend/models/Clase.js
+++ backend/models/Clase.js
@@ -4,6 +4,8 @@ const claseSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   horario: { type: String, required: true },
   estado: { type: String, default: "disponible" },
+  capacidadMaxima: { type: Number, required: true },
+  capacidadDisponible: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
