658f7495eff3b6919aa7c91a9b5b65443e53ce05
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-07-30 18:24:44 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,15 +3,11 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://gerencia:otF0Z8wGvnJdgm2T@cluster0.lvhzkqg.mongodb.net/porrismo?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://gerencia:otF0Z8wGvnJdgm2T@cluster0.lvhzkqg.mongodb.net/porras?retryWrites=true&w=majority&appName=Cluster0"
     );
-    console.log(`✅ MongoDB conectado: ${conn.connection.host} - DB: ${conn.connection.name}`);
+    console.log(`🟢 MongoDB conectado: ${conn.connection.host}`);
   } catch (error) {
-    console.error("❌ Error al conectar a MongoDB:", error.message);
+    console.error("🔴 Error al conectar a MongoDB:", error.message);
     process.exit(1);
   }
 };
