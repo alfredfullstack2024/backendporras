d55ae2e0f8b3e744fa3b6a0df3968e470a520ffd
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-07-30 11:51:48 -0500

    Update Cliente.js

--- backend/models/Cliente.js
+++ backend/models/Cliente.js
@@ -1,6 +1,6 @@
-const db = require("../config/db"); // Importar el objeto completo
+const mongoose = require("mongoose"); // Importar mongoose directamente
 
-const clienteSchema = new db.mongoose.Schema({
+const clienteSchema = new mongoose.Schema({
   numeroIdentificacion: { type: String, required: true, unique: true },
   nombre: { type: String, required: true },
   apellido: { type: String, required: true },
@@ -9,7 +9,7 @@ const clienteSchema = new db.mongoose.Schema({
   fechaNacimiento: { type: Date },
   fechaRegistro: { type: Date, default: Date.now },
   estado: { type: String, enum: ["activo", "inactivo"], default: "activo" },
-  membresias: [{ type: db.mongoose.Schema.Types.ObjectId, ref: "Membresia" }],
+  membresias: [{ type: mongoose.Schema.Types.ObjectId, ref: "Membresia" }],
 });
 
-module.exports = db.mongoose.model("Cliente", clienteSchema);
+module.exports = mongoose.model("Cliente", clienteSchema);
