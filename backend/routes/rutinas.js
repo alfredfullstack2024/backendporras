4f445edac32b939be01b5adeb94873205c73ff31
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-08-08 12:10:34 -0500

    Update rutinas.js

--- backend/routes/rutinas.js
+++ backend/routes/rutinas.js
@@ -1,62 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const rutinaController = require("../controllers/rutinasController");
-const { protect, verificarPermisos } = require("../middleware/authMiddleware");
+const rutinasController = require("../controllers/rutinasController");
+const { protect } = require("../middleware/authMiddleware");
 
-// Crear una rutina (Admin y Entrenador)
-router.post(
-  "/",
-  protect,
-  verificarPermisos(["admin", "entrenador"]),
-  rutinaController.crearRutina
-);
+// Rutas para gestionar rutinas
+router.post("/rutinas", protect, rutinasController.crearRutina);
+router.get("/rutinas", protect, rutinasController.listarRutinas);
+router.put("/rutinas/:id", protect, rutinasController.actualizarRutina);
 
-// Listar todas las rutinas (Admin y Entrenador)
-router.get(
-  "/",
-  protect,
-  verificarPermisos(["admin", "entrenador"]),
-  rutinaController.listarRutinas
-);
-
-// Actualizar una rutina (Solo admin)
-router.put(
-  "/:id",
-  protect,
-  verificarPermisos(["admin"]),
-  rutinaController.actualizarRutina
-);
-
-// Asignar una rutina a un cliente (Admin y Entrenador)
-router.post(
-  "/asignar",
-  protect,
-  verificarPermisos(["admin", "entrenador"]),
-  rutinaController.asignarRutina
-);
-
-// Actualizar una asignación de rutina (Admin y Entrenador)
-router.put(
-  "/asignar/:id",
-  protect,
-  verificarPermisos(["admin", "entrenador"]),
-  rutinaController.actualizarAsignacionRutina
-);
-
-// Eliminar una asignación de rutina (Solo admin)
-router.delete(
-  "/asignar/:id",
-  protect,
-  verificarPermisos(["admin"]),
-  rutinaController.eliminarAsignacionRutina
-);
-
-// Consultar todas las rutinas asignadas por número de identificación (Admin, Entrenador y Usuario)
-router.get(
-  "/consultarRutinasPorNumeroIdentificacion/:numeroIdentificacion",
-  protect,
-  verificarPermisos(["admin", "entrenador", "user"]),
-  rutinaController.consultarRutinasPorNumeroIdentificacion
-);
+// Rutas para gestionar asignaciones de rutinas
+router.post("/rutinas/asignar", protect, rutinasController.asignarRutina);
+router.put("/rutinas/asignar/:id", protect, rutinasController.actualizarAsignacionRutina);
+router.delete("/rutinas/asignar/:id", protect, rutinasController.eliminarAsignacionRutina);
+router.get("/rutinas/consultarRutinasPorNumeroIdentificacion/:numeroIdentificacion", protect, rutinasController.consultarRutinasPorNumeroIdentificacion);
 
 module.exports = router;
