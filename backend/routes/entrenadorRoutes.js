67a0943b81d89347d195731aa3cd74b5ad575319
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-08-10 11:43:21 -0500

    Update entrenadorRoutes.js

--- backend/routes/entrenadorRoutes.js
+++ backend/routes/entrenadorRoutes.js
@@ -9,19 +9,10 @@ const {
   eliminarEntrenador,
 } = require("../controllers/entrenadoresController");
 
-// Listar todos los entrenadores
 router.get("/", protect, listarEntrenadores);
-
-// Agregar un nuevo entrenador
-router.post("/", protect, agregarEntrenador);
-
-// Obtener un entrenador por ID
 router.get("/:id", protect, obtenerEntrenadorPorId);
-
-// Editar un entrenador existente
 router.put("/:id", protect, editarEntrenador);
-
-// Eliminar un entrenador
+router.post("/", protect, agregarEntrenador);
 router.delete("/:id", protect, eliminarEntrenador);
 
 module.exports = router;
