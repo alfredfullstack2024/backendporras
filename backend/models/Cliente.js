fbe713a19a3ca28d8f55aebec26d2931b9a5c063
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-09-08 13:38:16 -0500

    Update Cliente.js

--- backend/models/Cliente.js
+++ backend/models/Cliente.js
@@ -7,14 +7,14 @@ const clienteSchema = new mongoose.Schema({
   telefono: { type: String },
   email: { type: String },
   fechaNacimiento: { type: Date, required: true },
-  edad: { type: Number, required: true },
+  edad: { type: Number }, // ya NO es required, la calculamos en el frontend
   tipoDocumento: { type: String, enum: ["C.C", "T.I", "RC", "PPT"], default: "C.C", required: true },
   rh: { type: String },
   eps: { type: String },
   tallaTrenSuperior: { type: String },
   tallaTrenInferior: { type: String },
   nombreResponsable: { type: String },
-  direccion: { type: String }, // Aseguramos que esté definido
+  direccion: { type: String },
   fechaRegistro: { type: Date, default: Date.now },
   estado: { type: String, enum: ["activo", "inactivo"], default: "activo" },
   membresias: [{ type: mongoose.Schema.Types.ObjectId, ref: "Membresia" }],
